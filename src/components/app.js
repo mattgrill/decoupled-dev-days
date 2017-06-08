e4ddce2b7a59582d474dd2198c324de41bac1ecb
Matthew Grill <matt.grill@acquia.com>
2017-06-08 09:02:37 -0700

    Logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import {
   HashRouter as Router,
-  Route,
-  Link
 } from 'react-router-dom';
-
-import Home from './home';
-import About from './about';
-import Registration from './registration';
-import Discussions from './discussions';
-import Sponsors from './sponsors';
-import Venue from './venue';
-import News from './news';
 
 import '../styles/base.css';
 
+import Logo from './logo';
+
 const App = () => (
   <Router>
-    <div>
-      <h1>Decoupled Dev Days</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/registration">Registration</Link></li>
-        <li><Link to="/discussions">Discussions</Link></li>
-        <li><Link to="/sponsors">Sponsors</Link></li>
-        <li><Link to="/venue">Venue</Link></li>
-        <li><Link to="/news">News</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/registration" component={Registration}/>
-      <Route path="/discussions" component={Discussions}/>
-      <Route path="/sponsors" component={Sponsors}/>
-      <Route path="/venue" component={Venue}/>
-      <Route path="/news" component={News}/>
-
-    </div>
+    <Logo />
   </Router>
 );
 
