dd47999ffc213466ed235cb1793979f1e3adf61f
Matthew Grill <matt.grill@acquia.com>
2017-06-18 17:36:01 -0700

    Organize our components a bit better

--- src/components/app.js
+++ src/components/app.js
@@ -10,8 +10,16 @@ import Footer from './footer';
 
 const App = () => (
   <Router>
-    <div>
+    <div className="wrapper">
       <Logo />
+      <h1>Decoupled <span className="title-secondary">Developer Days</span></h1>
+      <div className="title-details">
+        <div className="title-location">New York City</div>
+        <div className="title-date">August 19&ndash;20, 2017</div>
+      </div>
+      <p>Decoupled Dev Days is a small developer-focused conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures.</p>
+      <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
+      <p>The mission of Decoupled Dev Days is threefold: share best practices in sessions, discuss difficult shared problems in discussions, and build open-source projects in sprints.</p>
       <Footer />
     </div>
   </Router>
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,25 +1,15 @@
 import React from 'react';
 
 const Logo = () => (
-  <div className="logo-wrapper">
-    <svg className="decoupled-dev-days-logo" data-name="Layer 7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 339.08 450.69">
-      <title>DecoupledDevDaysLogo</title>
-      <path className="cls-1" d="M509.33,475.67a169,169,0,1,0,263.27,0C721.92,411.2,671.19,381.68,641,302,610.75,381.68,560,411.2,509.33,475.67Z" transform="translate(-471.43 -300.59)" />
-      <path className="cls-2" d="M540.34,677.76a142.8,142.8,0,0,0,243.3-112.61H499A142.48,142.48,0,0,0,540.34,677.76Z" transform="translate(-471.43 -300.59)" />
-      <path className="cls-3" d="M783.64,565.16h-264a155.25,155.25,0,0,0,37.21,112.08c22.87,26.67,54,40.84,85.83,42.28l.93,0A142.69,142.69,0,0,0,783.34,591.68,207,207,0,0,0,783.64,565.16Z" transform="translate(-471.43 -300.59)" />
-      <path className="cls-4" d="M585.27,656.83a116.25,116.25,0,0,0,198.07-91.68H551.61A116,116,0,0,0,585.27,656.83Z" transform="translate(-471.43 -300.59)" />
-      <path className="cls-5" d="M725.91,505.93c-32.43-41.26-64.9-60.15-84.24-111.15-19.34,51-51.82,69.89-84.25,111.15A107.71,107.71,0,0,0,533.85,565H749.49A107.72,107.72,0,0,0,725.91,505.93Z" transform="translate(-471.43 -300.59)" />
-      <path className="cls-6" d="M749.49,565H533.85a108.18,108.18,0,1,0,215.64,0Z" transform="translate(-471.43 -300.59)" />
-    </svg>
-    <h1>Decoupled <span className="title-secondary">Developer Days</span></h1>
-    <div className="title-details">
-      <div className="title-location">New York City</div>
-      <div className="title-date">August 19&ndash;20, 2017</div>
-    </div>
-    <p>Decoupled Dev Days is a small developer-focused conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures.</p>
-    <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
-    <p>The mission of Decoupled Dev Days is threefold: share best practices in sessions, discuss difficult shared problems in discussions, and build open-source projects in sprints.</p>
-  </div>
+  <svg className="decoupled-dev-days-logo" data-name="Layer 7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 339.08 450.69">
+    <title>DecoupledDevDaysLogo</title>
+    <path className="cls-1" d="M509.33,475.67a169,169,0,1,0,263.27,0C721.92,411.2,671.19,381.68,641,302,610.75,381.68,560,411.2,509.33,475.67Z" transform="translate(-471.43 -300.59)" />
+    <path className="cls-2" d="M540.34,677.76a142.8,142.8,0,0,0,243.3-112.61H499A142.48,142.48,0,0,0,540.34,677.76Z" transform="translate(-471.43 -300.59)" />
+    <path className="cls-3" d="M783.64,565.16h-264a155.25,155.25,0,0,0,37.21,112.08c22.87,26.67,54,40.84,85.83,42.28l.93,0A142.69,142.69,0,0,0,783.34,591.68,207,207,0,0,0,783.64,565.16Z" transform="translate(-471.43 -300.59)" />
+    <path className="cls-4" d="M585.27,656.83a116.25,116.25,0,0,0,198.07-91.68H551.61A116,116,0,0,0,585.27,656.83Z" transform="translate(-471.43 -300.59)" />
+    <path className="cls-5" d="M725.91,505.93c-32.43-41.26-64.9-60.15-84.24-111.15-19.34,51-51.82,69.89-84.25,111.15A107.71,107.71,0,0,0,533.85,565H749.49A107.72,107.72,0,0,0,725.91,505.93Z" transform="translate(-471.43 -300.59)" />
+    <path className="cls-6" d="M749.49,565H533.85a108.18,108.18,0,1,0,215.64,0Z" transform="translate(-471.43 -300.59)" />
+  </svg>
 );
 
 export default Logo;
