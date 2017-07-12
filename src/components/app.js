a8f3c57a0b6a1e4e0b52e43bc1e21370f3fe6c24
Matt Davis <im.mr.jmd@gmail.com>
2017-07-12 10:06:12 -0700

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -16,6 +16,7 @@ const App = () => (
       <div className="title-details">
         <div className="title-location">New York City</div>
         <div className="title-date">August 19&ndash;20, 2017</div>
+        <div className="title-date"><a href="https://www.eventbrite.com/e/decoupled-developer-days-tickets-35731933174">Register to attend</a></div>
       </div>
       <p>Decoupled Dev Days is a small developer-focused conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures.</p>
       <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
