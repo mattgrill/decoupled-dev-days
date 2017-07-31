9cc8b9305542cdea832988f6dbdc1a169c90fe9b
kepford <bob@bobkepford.com>
2017-07-31 09:03:52 -0700

    Adds Fourkitchens sponsor
    * Fixes links to sponsor websites.

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -2,35 +2,36 @@ import React from 'react';
 import Pantheon from './sponsors/pantheon';
 import Acquia from './sponsors/acquia';
 import Mediacurrent from './sponsors/mediacurrent';
+import Fourkitchens from './sponsors/fourkitchens';
 import Lullabot from './sponsors/lullabot';
 
 const Sponsors = () => (
   <div className="sponsors">
     <h1>Sponsors</h1>
-    <h2>Diamond Sponsor</h2>
-    <a href="http://lullabot.com"><img src="assets/sponsors/pantheon.svg" alt="Pantheon" title="Pantheon"/></a>
-    <p>This is a placeholder</p>
 
     <h2>Gold Sponsors</h2>
     <div className="gold">
       <div className="sponsor">
-        <a href="http://acquia.com"><Acquia /></a>
+        <a href="https://www.acquia.com/"><Acquia /></a>
       </div>
       <div className="sponsor">
-        <a href="http://phase2.com"><img src="assets/sponsors/phase2.png" alt="Phase2" title="Phase2"/></a>
+        <a href="https://www.mediacurrent.com/"><Mediacurrent /></a>
       </div>
       <div className="sponsor">
-        <a href="http://mediacurrent.com"><Mediacurrent /></a>
+        <a href="https://www.phase2technology.com/"><img src="assets/sponsors/phase2.png" alt="Phase2" title="Phase2"/></a>
       </div>
     </div>
 
     <h2>Silver Sponsors</h2>
     <div className="silver">
       <div className="sponsor">
-        <a href="http://pantheon.io"><Pantheon /></a>
+        <a href="https://www.fourkitchens.com/"><Fourkitchens /></a>
       </div>
       <div className="sponsor">
-        <a href="http://lullabot.com"><Lullabot /></a>
+        <a href="https://www.lullabot.com/"><Lullabot /></a>
+      </div>
+      <div className="sponsor">
+        <a href="https://pantheon.io/"><Pantheon /></a>
       </div>
     </div>
   </div>
