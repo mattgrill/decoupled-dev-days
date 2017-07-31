e62bf1df4a2c7b40539c08102055493d4d1a7727
kepford <bob@bobkepford.com>
2017-07-31 09:03:52 -0700

    Adds Fourkitchens sponsor
    * Fixes links to sponsor websites.

---
+++ src/components/sponsors/fourkitchens.js
@@ -0,0 +1,9 @@
+import React from 'react';
+var Isvg = require('react-inlinesvg');
+
+const Fourkitchens = () => (
+  <Isvg src="assets/sponsors/fourkitchens.svg">
+  </Isvg>
+);
+
+export default Fourkitchens;
