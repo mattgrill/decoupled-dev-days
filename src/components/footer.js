52c6473a96f1062506fbbb961c4bdcbbc9e139af
Preston So <pso@post.harvard.edu>
2017-08-05 15:25:21 -0400

    Replace Link with NavLink in footer nav to facilitate active trail.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {
-  Link
+  Link,
+  NavLink
 } from 'react-router-dom';
 
 const Footer = () => (
   <footer>
     <nav className="footer-first">
       <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/schedule">Schedule</Link></li>
-        <li><Link to="/venue">Venue</Link></li>
-        <li><Link to="/sponsors">Sponsors</Link></li>
-        <li><Link to="/sponsoring">Sponsoring</Link></li>
+        <li><NavLink to="/about" activeClassName="is-active">About</NavLink></li>
+        <li><NavLink to="/schedule" activeClassName="is-active">Schedule</NavLink></li>
+        <li><NavLink to="/venue" activeClassName="is-active">Venue</NavLink></li>
+        <li><NavLink to="/sponsors" activeClassName="is-active">Sponsors</NavLink></li>
+        <li><NavLink to="/sponsoring" activeClassName="is-active">Sponsoring</NavLink></li>
         <li><a href="http://bit.ly/2tJh1EL" target="_blank" rel="noopener noreferrer" className="register-link">Register</a></li>
       </ul>
     </nav>
