452e653f68ff1c23e2980f807a438ec5d84ac69f
Matt Davis <matt.davis@mediacurrent.com>
2017-08-06 15:07:22 -0700

    Unstyled speakers section

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Speakers from './speakers';
 
 const Index = () => (
   <div className="index">
@@ -7,6 +8,7 @@ const Index = () => (
       <p>Decoupled Dev Days is a small developer-focused conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures.</p>
       <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
       <p>The mission of Decoupled Dev Days is threefold: share best practices in sessions, discuss difficult shared problems in discussions, and build open-source projects in sprints.</p>
+      <Speakers />
     </div>
   </div>
 );
---
+++ src/components/speakers.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const speakerData = [
+  {
+    name: 'Stephen Fluin',
+    role: 'Angular Team',
+    photo: 'assets/speakers/stephen.jpg',
+  },
+  {
+    name: 'Ed Faulkner',
+    role: 'Ember Team',
+    photo: 'assets/speakers/ed.jpg',
+  },
+  {
+    name: 'Sally Young',
+    role: 'Contenta Team',
+    photo: 'assets/speakers/sally.jpg',
+  },
+  {
+    name: 'Ted Bowman',
+    role: 'Reservoir Team',
+    photo: 'assets/speakers/ted.jpg',
+  },
+];
+
+const Speakers = () => (
+  <section className="speakers">
+    <h2>Featured Speakers</h2>
+    {speakerData.map(speaker => (
+      <section key={speaker.name} className="speaker">
+        <p>{speaker.name}</p>
+        <p>{speaker.role}</p>
+        <img src={speaker.photo} alt={speaker.name} />
+      </section>
+    ))}
+  </section>
+);
+
+export default Speakers;
