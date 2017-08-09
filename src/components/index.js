99a130b33936c261138baddeed2caea2f002b8fe
Preston So <pso@post.harvard.edu>
2017-08-09 17:45:15 -0300

    Add CTA to home page.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
 
 const Index = () => (
   <div className="index">
@@ -7,6 +10,7 @@ const Index = () => (
       <p>Decoupled Dev Days is a small developer-focused conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures.</p>
       <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
       <p>The mission of Decoupled Dev Days is threefold: share best practices in sessions, discuss difficult shared problems in discussions, and build open-source projects in sprints.</p>
+      <p><Link to="/about">Learn more</Link></p>
     </div>
   </div>
 );
